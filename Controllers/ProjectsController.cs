1dc7ead10f22061f76ed180c7ea51f1c3fb39a74
Jossany Silva <jossanysilva@USERs-MacBook-Pro.local>
2023-10-28 16:11:11 -0300

    Create UserController and modify ProjectsController

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -48,6 +48,24 @@ namespace DevFreela.API.Controllers
 		{
 			return NoContent();
 		}
-	}
+
+		[HttpPost("{id}/comments")]
+		public IActionResult PostComment(int id, [FromBody] CreateCommentModel createCommentModel)
+		{
+			return NoContent();
+		}
+
+		[HttpPut("{id}/start")]
+		public IActionResult Start(int id)
+		{
+			return NoContent();
+		}
+
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+    }
 }
 
